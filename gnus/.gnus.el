bdded7969bb9e8dba9ff3499b745247787ca4c5e
Antoine Martin <antoine97.martin@gmail.com>
2020-09-15 19:48:49 +0200

    gnus: use login in news address

--- gnus/.gnus.el
+++ gnus/.gnus.el
@@ -1,3 +1,3 @@
-(setq user-mail-address "antoine4.martin@epita.fr"
+(setq user-mail-address "ma_9@epita.fr"
       user-full-name    "Antoine Martin")
 (setq gnus-select-method '(nntp "news.epita.fr"))
